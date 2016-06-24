4762b29e88e7c4f64d1aa2c370f36944eee7b0af
rafaelsclaus <rafaelclaus331@gmail.com>
2016-06-24 20:44:12 -0300

    Add RequestDispatcher and Interface Logica

--- fj21-agenda/src/br/com/caelum/servlet/AdicionaContatoServlet.java
+++ fj21-agenda/src/br/com/caelum/servlet/AdicionaContatoServlet.java
@@ -7,6 +7,7 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -56,12 +57,12 @@ public class AdicionaContatoServlet extends HttpServlet {
 		ContatoDao dao = new ContatoDao();
 		dao.adiciona(contato);
 		
-		//imprime o nome do contato que foi adicionado
-		out.println("<html>");
-		out.println("<body>");
-		out.println("Contato " + contato.getNome() + " adicionado com sucesso");
-		out.println("</body>");
-		out.println("</html>");
+		RequestDispatcher rd = request.getRequestDispatcher("contato-adicionado.jsp");
+		
+		rd.forward(request, response);
+		
+		
+		
 		
 		
 		
