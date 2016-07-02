3cff754c1dd85c6e66041a0ac7709eb8636438be
rafaelsclaus <rafaelclaus331@gmail.com>
2016-07-01 23:03:08 -0300

    Buscando com JPQL.

--- fj21-tarefas/src/br/com/caelum/tarefas/jpa/CarregaTarefa.java
+++ fj21-tarefas/src/br/com/caelum/tarefas/jpa/CarregaTarefa.java
@@ -1,27 +1,23 @@
 package br.com.caelum.tarefas.jpa;
 
-import java.util.Calendar;
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 import br.com.caelum.tarefas.modelo.Tarefa;
 
-public class AdicionaTarefa {
+public class CarregaTarefa {
+
 	public static void main(String[] args) {
-		Tarefa tarefa = new Tarefa();
-		tarefa.setDescricao("Claussssssss");
-		tarefa.setFinalizado(true);
-		tarefa.setDataFinalizacao(Calendar.getInstance());
 		
 		EntityManagerFactory factory = Persistence.createEntityManagerFactory("tarefas");
 		
 		EntityManager mananger = factory.createEntityManager();
 		
-		mananger.getTransaction().begin();;
-		mananger.persist(tarefa);
-		mananger.getTransaction().commit();
+		
+		Tarefa encontrada = mananger.find(Tarefa.class, 1L);
+		
+		System.out.println(encontrada.getDescricao());
 		
 		mananger.close();
 	}
